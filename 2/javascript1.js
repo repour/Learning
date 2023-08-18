b393067f64515d2f784b5ddcd2c2638b94e36e09
repour <125547584+repour@users.noreply.github.com>
2023-08-18 21:26:20 +0330

    Update javascript1.js

--- 2/javascript1.js
+++ 2/javascript1.js
@@ -1,4 +1,5 @@
 const firstName = prompt("please enter your first name");
 const lastName = prompt("Please enter your last name");
-console.log(firstName + " " + lastName);
+const fullName = firstName + " " + lastName;
+console.log(fullName);
 console.log(firstName.length +lastName.length);
