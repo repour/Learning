65479b5a9583288b052d6f39dcadca3baf1e464a
repour <125547584+repour@users.noreply.github.com>
2023-08-18 23:59:00 +0330

    Update javascript.js

--- 1/javascript.js
+++ 1/javascript.js
@@ -1,6 +1,6 @@
 const gettingNumber = prompt("please enter your number");
-let printNumber = 0;
+let printNumber = [];
 for (let i = 0; i <= gettingNumber; i += 2) {
-    console.log(printNumber);
-    printNumber += 2;
+    printNumber.push(i)
 }
+console.log(printNumber.lenght);
