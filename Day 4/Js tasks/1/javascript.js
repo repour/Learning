68eba4d8237c3f6ebd5ec48ace3d51f0c73a65af
repour <125547584+repour@users.noreply.github.com>
2023-08-22 10:47:50 +0330

    Update javascript.js

--- Day 4/Js tasks/1/javascript.js
+++ Day 4/Js tasks/1/javascript.js
@@ -1,6 +1,5 @@
 const getNumber = prompt("please enter a number!!");
 if (getNumber > 2) {
-    let i;
     let listOfRemainder = [];
     for (let i = 2; i < getNumber; i++) {
         listOfRemainder.push(getNumber % i);
