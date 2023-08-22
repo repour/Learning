096a7d4ebcf7d5c9c0d8f9433eface78695e9a68
repour <125547584+repour@users.noreply.github.com>
2023-08-22 11:10:34 +0330

    Update javascript.js

--- Day 4/Js tasks/4/javascript.js
+++ Day 4/Js tasks/4/javascript.js
@@ -1,1 +1,10 @@
-const getString = prompt("please enter some text!!");
+const getText = prompt("please enter some text!!");
+let arryText = getText.split("");
+let dupArrText = [];
+for (let i = 0; i < arryText.length; i++) {
+    const element = arryText[i].repeat(2);
+    dupArrText.push(element);
+}
+let dupText = dupArrText.join("");
+console.log(dupArrText);
+console.log(dupText);
