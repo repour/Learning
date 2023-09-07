7567261feb1f7b9d2f4dee7902d6f8db6cd30e10
repour <125547584+repour@users.noreply.github.com>
2023-09-07 11:08:45 +0330

    Update javascript.js

--- Day 8/JS/03/javascript.js
+++ Day 8/JS/03/javascript.js
@@ -6,14 +6,11 @@ console.log("Second array", getArry2);
 function uniqueItemInArry(arry1, arry2) {
     //////////// in this section we can find items in first arry///////////////
     let firstResult = [...arry1];
-    for (let j = 0; j < arry2.length; j++) {
-        firstResult = firstResult.filter((item) => item !== arry2[j]);
-    }
+        firstResult = firstResult.filter((item) => !arry2.includes(item));
+
     //////////// in this section we can find items in first arry///////////////
     let secondResult = [...arry2];
-    for (let j = 0; j < arry1.length; j++) {
-        secondResult = secondResult.filter((item) => item !== arry1[j]);
-    }
+        secondResult = secondResult.filter((item) => !arry1.includes(item));
 
     const finalResult = [...firstResult, ...secondResult];
     console.log("Result", finalResult);
